369cdcfeaec2a6babf228f4df6d0551b49bbb5a8
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-01-27 17:36:41 +0530

    Added solution - LeetHub

---
+++ Vertical sum - GFG/vertical-sum.java
@@ -0,0 +1,152 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			
+			    ArrayList <Integer> res = g.verticalSum(root);
+			    for (Integer num : res) System.out.print (num + " ");
+			    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+int data;
+Node left, right;
+Node(int item)
+{
+    data = item;
+    left = right = null
+}
+}
+*/
+class Solution
+{
+    static class pair
+    {
+        Node node;
+        int level;
+        pair(Node node, int level)
+        {
+            this.node = node;
+            this.level = level;
+        }
+    }
+    
+    public ArrayList<Integer> verticalSum(Node root) 
+    {
+        Queue<pair> q = new LinkedList<>();
+        if(root != null)
+            q.add(new pair(root, 0));
+        TreeMap<Integer, Integer> tp = new TreeMap<>();
+        while(!q.isEmpty())
+        {
+            Node node = q.peek().node;
+            int level = q.peek().level;
+            q.poll();
+            if(tp.containsKey(level))
+                tp.put(level, tp.get(level) + node.data);
+            else
+                tp.put(level, node.data);
+            if(node.left != null)
+                q.add(new pair(node.left, level-1));
+            if(node.right != null)
+                q.add(new pair(node.right, level+1));
+        }
+        ArrayList<Integer> levelSum = new ArrayList<>();
+        for(int l: tp.keySet())
+            levelSum.add(tp.get(l));
+        return levelSum;
+    }
+}
