ec3677d084fc548637fe3027e7b25d28d200e5cd
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-01-29 17:21:52 +0530

    Added solution - LeetHub

---
+++ The New Line - GFG/the-new-line.c
@@ -0,0 +1,27 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+
+void printNewLine();
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+// Function to print each word in new line
+void printNewLine()
+{
+    printf("Geeks\n");
+    printf("for\n");
+    printf("Geeks\n");
+}
+
+// { Driver Code Starts.
+
+int main(){
+    
+    //function call
+    printNewLine();
+    
+}  // } Driver Code Ends
