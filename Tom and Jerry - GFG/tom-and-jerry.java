624ce97b2682c3001fc47de741a32ed71f2aef02
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-02-01 19:50:44 +0530

    Added solution - LeetHub

---
+++ Tom and Jerry - GFG/tom-and-jerry.java
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.numsGame(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution 
+{
+    static int numsGame(int N)
+    {
+        if((N & 1) == 0)
+            return 1;
+        return 0;
+    }
+}
