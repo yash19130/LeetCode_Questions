ddda7aa6a1df53f303fc94deb1417c02a6cf0d12
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-02-02 21:50:03 +0530

    Time: 237 ms (12.08%), Space: 130.4 MB (5.19%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,18 +1,28 @@
 class Solution 
 {
+    static class pair
+    {
+        int e, i;
+        pair(int e, int i)
+        {
+            this.e = e;
+            this.i = i;
+        }
+    }
+    
     public int[] dailyTemperatures(int[] t) 
     {
         int n = t.length;
         int[] ans = new int[n];
-        Stack<int[]> s = new Stack<>();
+        Stack<pair> s = new Stack<>();
         ans[n-1] = 0;
-        s.push(new int[]{t[n-1], n-1});
+        s.push(new pair(t[n-1], n-1));
         for(int i=n-2; i>=0; i--)
         {
-            while(!s.isEmpty() && s.peek()[0] <= t[i])
+            while(!s.isEmpty() && s.peek().e <= t[i])
                 s.pop();
-            ans[i] = (s.isEmpty()) ? 0 : s.peek()[1] - i;
-            s.push(new int[]{t[i], i});
+            ans[i] = (s.isEmpty()) ? 0 : s.peek().i - i;
+            s.push(new pair(t[i], i));
         }
         return ans;
     }
