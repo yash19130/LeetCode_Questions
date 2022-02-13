33c11688b756c89e6f34f41dc0803059f8f5489c
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-02-14 01:25:10 +0530

    Time: 80 ms (66.67%), Space: 56.3 MB (66.67%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.java
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.java
@@ -2,57 +2,44 @@ class Solution
 {
     public int minimumOperations(int[] nums) 
     {
+        int n = nums.length;
         Map<Integer, Integer> odd = new HashMap<>();
         Map<Integer, Integer> even = new HashMap<>();
-        int e = 0, o = 0;
-        int n = nums.length;
         if(n == 1)
             return 0;
         for(int i=0; i<n; i++)
         {
             if(i % 2 == 0)
-            {
-                e++;
                 even.put(nums[i], even.getOrDefault(nums[i], 0) + 1);
-            }
             else
-            {
-                o++;
                 odd.put(nums[i], odd.getOrDefault(nums[i], 0) + 1);
+        }
+        int oddMax = 0, oddMaxVal = 0, OddMaxS = 0;
+        for(int k: odd.keySet())
+        {
+            if(odd.get(k) > oddMaxVal)
+            {
+                OddMaxS = oddMaxVal;
+                oddMax = k;
+                oddMaxVal = odd.get(k);
             }
+            else if(odd.get(k) > OddMaxS)
+                OddMaxS = odd.get(k);
         }
-        int maxOdd[] = {0, 0};
-        Queue<Map.Entry<Integer, Integer>> pqO = new PriorityQueue<>((a,b)->(b.getValue()-a.getValue()));
-        for(Map.Entry<Integer, Integer> i: odd.entrySet())
-            pqO.add(i);
-        Queue<Map.Entry<Integer, Integer>> pqE = new PriorityQueue<>((a,b)->(b.getValue()-a.getValue()));
-        for(Map.Entry<Integer, Integer> i: even.entrySet())
-            pqE.add(i);
-        Map.Entry<Integer, Integer> e1 = null, e2 = null;
-        if(!pqO.isEmpty() && !pqE.isEmpty() && pqO.peek().getKey() == pqE.peek().getKey())
-             e1 = pqO.remove();
-        if(pqO.isEmpty())
+        int evenMax = 0, evenMaxVal = 0, EvenMaxS = 0;
+        for(int k: even.keySet())
         {
-            e2 = pqE.remove();
-            if(pqE.isEmpty())
-                return o;
-            return e - pqE.peek().getValue();
+            if(even.get(k) > evenMaxVal)
+            {
+                EvenMaxS = evenMaxVal;
+                evenMax = k;
+                evenMaxVal = even.get(k);
+            }
+            else if(even.get(k) > EvenMaxS)
+                EvenMaxS = even.get(k);
         }
-        int val1 = (e - pqE.peek().getValue()) + (o - pqO.peek().getValue());
-        if(e1 != null)
-            pqO.add(e1);
-        if(e2 != null)
-            pqE.offer(e2);
-        if(!pqO.isEmpty() && !pqE.isEmpty() && pqO.peek().getKey() == pqE.peek().getKey())
-            pqE.remove();
-        if(pqE.isEmpty())
-        {
-            pqO.remove();
-            if(pqO.isEmpty())
-                return e;
-            return o - pqO.peek().getValue();
-        }
-        int val2 = (e - pqE.peek().getValue()) + (o - pqO.peek().getValue());
-        return Math.min(val1, val2);
+        if(oddMax != evenMax)
+            return n - oddMaxVal - evenMaxVal;
+        return Math.min(n - oddMaxVal - EvenMaxS, n - evenMaxVal - OddMaxS);
     }
 }
