4116dfb5f02363cf904b5bb4f69b71c44e66aa4a
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-02-18 00:20:35 +0530

    Time: 13 ms (15.56%), Space: 45.1 MB (17.16%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    List<List<Integer>> ans = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum(int[] a, int target) 
+    {
+        Arrays.sort(a);
+        solve(0, a, target, 0, new ArrayList<>());
+        return ans;
+    }
+    
+    public void solve(int i, int a[], int target, int sum, List<Integer> l)
+    {
+        if(sum > target)
+            return;
+        if(sum == target)
+        {
+            i++;
+            ans.add(new ArrayList<>(l));
+        }
+        for(int j=i; j<a.length; j++)
+        {
+            l.add(a[j]);
+            solve(j, a, target, sum + a[j], l);
+            l.remove(l.size()-1);
+        }
+    }
+}
