083ae3a6fd392140c87a18f171279b7fd2e47ab7
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-02-23 17:58:35 +0530

    Time: 42 ms (48.22%), Space: 43.1 MB (29.86%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -1,11 +1,48 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
 
-class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        return deepcopy(node)
+class Solution 
+{
+    public void dfs(Node v, Node cur, Node[] vis)
+    {
+        vis[cur.val] = cur;
+        for(Node n: v.neighbors)
+        {
+            if(vis[n.val] == null)
+            {
+                Node node = new Node(n.val);
+                dfs(n, node, vis);
+                cur.neighbors.add(node);
+            }
+            else
+                cur.neighbors.add(vis[n.val]);
+        }
+    }
+    
+    public Node cloneGraph(Node node) 
+    {
+        if(node == null)
+            return null;
+        Node[] vis = new Node[101];
+        Node ans = new Node(node.val);
+        dfs(node, ans, vis);
+        return ans;
+    }
+}
