b8722ad9f64c5dd8334eef77476b7c6d8cf82af7
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-02-26 00:45:20 +0530

    Added solution - LeetHub

---
+++ Sort a String - GFG/sort-a-string.java
@@ -0,0 +1,27 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine().trim());
+        while(t-- > 0){
+            String s = sc.nextLine().trim();
+            Solution obj = new Solution();
+            System.out.println(obj.sort(s));
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution 
+{ 
+    String sort(String s) 
+    {
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        return String.valueOf(arr);
+    }
+} 
