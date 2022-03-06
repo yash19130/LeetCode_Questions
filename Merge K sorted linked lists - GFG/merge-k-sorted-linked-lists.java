6ce1302b73bbd3342c6b4cced0c792c7354a4a31
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-03-07 00:57:36 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.java
@@ -0,0 +1,97 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution
+{
+    Node mergeKList(Node[] arr, int K)
+    {
+        Queue<Node> pq = new PriorityQueue<>((a, b) -> (a.data - b.data));
+        for(Node head: arr)
+            pq.add(head);
+        Node sentinel = new Node(0);
+        Node head = sentinel;
+        while(!pq.isEmpty())
+        {
+            Node node = pq.poll();
+            sentinel.next = node;
+            sentinel = sentinel.next;
+            if(node.next != null)
+                pq.add(node.next);
+        }
+        return head.next;
+    }
+}
