a8f1eef1a71bf9ca09c2313db93d7445887e46d9
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-04-29 00:36:07 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.java
@@ -0,0 +1,97 @@
+// { Driver Code Starts
+// driver code
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            System.out.println( x.countNodesinLoop(head) );
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+class Solution
+{
+    static int countNodesinLoop(Node head)
+    {
+        Node temp = head;
+        HashSet<Node> hs = new HashSet<>();
+        while(temp!=null)
+        {
+            if(hs.contains(temp))
+            {
+                Node node = temp;
+                int c = 0;
+                do
+                {
+                    node = node.next;
+                    c++;
+                }while(node!=temp);
+                return c;
+            }
+            hs.add(temp);
+            temp = temp.next;
+        }
+        return 0;
+    }
+}
