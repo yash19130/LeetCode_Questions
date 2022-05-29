bb29f135a037e02a8679945d46b20d29ab898f02
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-05-29 15:22:31 +0530

    Time: 367 ms (20.99%), Space: 72.2 MB (16.21%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -4,24 +4,27 @@ class Solution
     {
         int n = words.length;
         int maxP = 0;
-        Map<String, int[]> hp = new HashMap<>();
+        Map<String, String> hp = new HashMap<>();
         for(String w: words)
         {
             int[] cnt = new int[26];
             for(char c: w.toCharArray())
                 cnt[c - 'a']++;
-            hp.put(w, cnt);
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<26; i++)
+                sb.append(cnt[i] + " ");
+            hp.put(w, sb.toString());
         }
         for(int i=0; i<n; i++)
         {
             for(int j=i+1; j<n; j++)
             {
-                int[] cnt1 = hp.get(words[i]);
-                int[] cnt2 = hp.get(words[j]);
+                String s1 = hp.get(words[i]);
+                String s2 = hp.get(words[j]);
                 boolean shareCommon = false;
-                for(int k=0; k<26; k++)
+                for(int k=0; k<52; k+=2)
                 {
-                    if(cnt1[k] > 0 && cnt2[k] > 0)
+                    if(s1.charAt(k) != '0' && s2.charAt(k) != '0')
                     {
                         shareCommon = true;
                         break;
