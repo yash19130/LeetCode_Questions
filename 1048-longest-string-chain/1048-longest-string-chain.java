faf9485733ff67c0caf9f050d727b8493a20f51b
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-06-15 14:30:01 +0530

    Time: 60 ms (57.51%), Space: 72.5 MB (5.98%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -6,18 +6,19 @@ class Solution
         Arrays.sort(words, (a, b) -> (b.length() - a.length()));
         for(String s: words)
             hp.put(s, 1);
+        int longest = 1;
         for(String s: words)
         {
             for(int i=0; i<s.length(); i++)
             {
                 String word = s.substring(0, i) + s.substring(i + 1);
                 if(hp.containsKey(word))
+                {
                     hp.put(word, Math.max(hp.get(word), hp.get(s) + 1));
+                    longest = Math.max(longest, hp.get(word));
+                }
             }
         }
-        int longest = 1;
-        for(String s: hp.keySet())
-            longest = Math.max(longest, hp.get(s));
         return longest;
     }
 }
