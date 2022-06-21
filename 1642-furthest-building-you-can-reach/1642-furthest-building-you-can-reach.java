e1017843b060224baa7333fe6e168fac0d5f207e
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-06-21 20:30:24 +0530

    Time: 37 ms (28.65%), Space: 70.8 MB (69.28%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -3,57 +3,20 @@ class Solution
     public int furthestBuilding(int[] h, int bricks, int ladders) 
     {
         int n = h.length;
-        Queue<int[]> bricksNeeded = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        Queue<Integer> pq = new PriorityQueue<>();
         for(int i=1; i<n; i++)
         {
-            if(h[i] > h[i - 1])
-            {
-                if(bricksNeeded.size() < ladders)
-                {
-                    bricksNeeded.add(new int[]{h[i] - h[i - 1], i});
-                }
-                else if(!bricksNeeded.isEmpty() && bricksNeeded.peek()[0] < h[i] - h[i - 1])
-                {
-                    bricksNeeded.poll();
-                    bricksNeeded.add(new int[]{h[i] - h[i - 1], i});
-                }
-            }
-        }
-        TreeSet<Integer> hs = new TreeSet<>();
-        while(ladders > 0 && bricksNeeded.size() > 0)
-        {
-            hs.add(bricksNeeded.poll()[1]);
-            ladders--;
-        }
-        int i = 1;
-        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
-        while(i < n)
-        {
-            if(h[i] <= h[i - 1] || hs.contains(i))
-            {
-                i++;
+            if(h[i] <= h[i - 1])
                 continue;
-            }
-            if(bricks >= h[i] - h[i - 1])
+            pq.add(h[i] - h[i - 1]);
+            if(pq.size() > ladders)
             {
-                bricks -= (h[i] - h[i - 1]);
-                pq.add(h[i] - h[i - 1]);
+                if(bricks >= pq.peek())
+                    bricks -= pq.poll();
+                else
+                    return i - 1;
             }
-            else
-            {
-                if(hs.isEmpty() || hs.last() < i)
-                    break;
-                hs.remove(hs.last());
-                if(!pq.isEmpty() && pq.peek() > h[i] - h[i - 1])
-                {
-                    bricks += pq.peek();
-                    pq.poll();
-                    bricks -= h[i] - h[i - 1];
-                    pq.add(h[i] - h[i - 1]);
-                }
-            }
-            i++;
         }
-        return i - 1;
+        return n - 1;
     }
 }
