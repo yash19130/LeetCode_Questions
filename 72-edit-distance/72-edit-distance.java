468402d6f54f779c5d5addcc84edfa5907d710f8
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-06-23 20:09:08 +0530

    Time: 11 ms (28.16%), Space: 45.4 MB (14.54%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -3,22 +3,23 @@ class Solution
     public int minDistance(String s1, String s2) 
     {
         int n = s1.length(), m = s2.length();
-        Integer[][] dp = new Integer[n][m];
-        return solve(0, 0, s1, s2, dp);
+        int[][] dp = new int[n + 1][m + 1];
+        dp[0][0] = 0;
+        for(int i=1; i<=n; i++)
+            dp[i][0] = i;
+        for(int j=1; j<=m; j++)
+            dp[0][j] = j;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+        return dp[n][m];
     }
     
-    public int solve(int i, int j, String s1, String s2, Integer[][] dp)
-    {
-        int n = s1.length(), m = s2.length();
-        if(i == n)
-            return m - j;
-        if(j == m)
-            return n - i;
-        if(dp[i][j] != null)
-            return dp[i][j];
-        if(s1.charAt(i) == s2.charAt(j))
-            return dp[i][j] = solve(i + 1, j + 1, s1, s2, dp);
-        return dp[i][j] = 1 + Math.min(solve(i + 1, j + 1, s1, s2, dp), 
-                        Math.min(solve(i + 1, j, s1, s2, dp), solve(i, j + 1, s1, s2, dp)));
-    }
 }
