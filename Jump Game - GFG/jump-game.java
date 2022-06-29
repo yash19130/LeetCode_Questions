be1f3ba808d13249e7d583f80d1a614cfac03450
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-06-29 23:58:40 +0530

    Added solution - LeetHub

---
+++ Jump Game - GFG/jump-game.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S1[] = read.readLine().split(" ");
+            
+            int[] A = new int[N];
+            
+            for(int i=0; i<N; i++)
+                A[i] = Integer.parseInt(S1[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.canReach(A,N));
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution 
+{
+    static int canReach(int[] a, int n) 
+    {
+        int max = 0;
+        if(a[0]==0)
+            return 0;
+        for(int i=0; i<n; i++)
+        {
+            if(a[i]>0)
+                max = Math.max(max, i+a[i]);
+            if(max>=n-1)
+                break;
+            else
+            {
+                if(max<=i)
+                    return 0;
+            }
+        }
+        if(max>=n-1)
+            return 1;
+        return 0;
+    }
+}
