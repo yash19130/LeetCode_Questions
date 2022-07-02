dd81604def0133594c0de74ce7680fc64fb38d02
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-07-03 01:34:17 +0530

    Time: 1 ms (59.78%), Space: 42.6 MB (31.43%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -3,7 +3,6 @@ class Solution
     public void gameOfLife(int[][] board) 
     {
         int m = board.length, n = board[0].length;
-        int[][] temp = new int[m][n];
         int[] dx = {0, 1, -1, 0, -1, -1, 1, 1};
         int[] dy = {1, 0, 0, -1, -1, 1, -1, 1};
         for(int i=0; i<m; i++)
@@ -21,20 +20,29 @@ class Solution
                 if(board[i][j] == 1)
                 {
                     if(liveNeighbors == 2 || liveNeighbors == 3)
-                        temp[i][j] = 1;
+                        board[i][j] = 1;
+                    else
+                        board[i][j] = -1;
                 }
                 else if(liveNeighbors == 3)
-                    temp[i][j] = 1;
+                    board[i][j] = 2;
             }
         }
         for(int i=0; i<m; i++)
+        {
             for(int j=0; j<n; j++)
-                board[i][j] = temp[i][j];                
+            {
+                if(board[i][j] == -1)
+                    board[i][j] = 0;
+                else if(board[i][j] == 2)
+                    board[i][j] = 1;
+            }
+        }
     }
     
     public boolean isLiveNeighbor(int i, int j, int[][] board)
     {
         int m = board.length, n = board[0].length;
-        return i >= 0 && i < m && j >= 0 && j < n && board[i][j] == 1;
+        return i >= 0 && i < m && j >= 0 && j < n && Math.abs(board[i][j]) == 1;
     }
 }
