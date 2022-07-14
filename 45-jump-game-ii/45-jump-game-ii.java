ff04bd6823602e73ecfe41af9a812dc2fe037a9b
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-07-14 11:16:08 +0530

    Time: 3 ms (55.19%), Space: 49.6 MB (35.87%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,6 +1,6 @@
-class Solution 
+class Solution
 {
-    public int jump(int[] nums) 
+    public int jump(int[] nums)
     {
         int jumps = 0;
         int n = nums.length;
@@ -17,33 +17,3 @@ class Solution
         return jumps;
     }
 }
-/*
-    [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]
-
-0) farthestTillNow = Math.max(farthestTillNow, 0 + 7) = 7
-    0 == end:
-        jumps++
-        end = farthestTillNow
-1) farthestTillNow = Math.max(farthestTillNow, 1 + 0) = 7
-    1 != end
-2) farthestTillNow = Math.max(farthestTillNow, 2 + 9) = 11
-    2 != end
-3) farthestTillNow = Math.max(farthestTillNow, 3 + 6) = 11
-...
-7) farthestTillNow = Math.max(farthestTillNow, 7 + 7) = 14
-    7 == end:
-        jumps++;
-        end = farthestTillNow
-        
-    [2,3,1,1,4]
-0) farthestTillNow = Math.max(farthestTillNow, 0 + 2) = 2
-    0 == end:
-        jumps++;
-        end = farthestTillNow
-1) farthestTillNow = Math.max(farthestTillNow, 1 + 3) = 4
-    1 != end
-2) farthestTillNow = Math.max(farthestTillNow, 2 + 1) = 4
-    2 == end:
-        jumps++;
-        end = farthestTillNow = 4
-*/
