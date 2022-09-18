6b20711ca650f211bc112c9fdc2ad004786f2870
yash19130 <73228640+yash19130@users.noreply.github.com>
2022-09-18 18:00:50 +0530

    Time: 3 ms (50.00%), Space: 6 MB (50.00%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,12 @@
+class Solution 
+{
+public:
+    int smallestEvenMultiple(int n) 
+    {
+        if(n <= 2)
+            return 2;
+        if(n % 2 == 0)
+            return n;
+        return n * 2;
+    }
+};
